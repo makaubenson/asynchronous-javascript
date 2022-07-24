855c6a058ec5823d352973e11c353c224b325d5c
Benson <bensonmakau2000@gmail.com>
2022-07-24 11:46:39 +0300

    fetching data and displaying in html

--- script.js
+++ script.js
@@ -20,4 +20,18 @@ request.addEventListener('load', function () {
   //convert data to js object from JSON
   const [data] = JSON.parse(this.responseText);
   console.log(data);
+
+  const html = `
+  <article class="country">
+  <img class="country__img" src="${data.flags.png}" />
+  <div class="country__data">
+    <h3 class="country__name">${data.name.common}</h3>
+    <h4 class="country__region">${data.region}</h4>
+    <p class="country__row"><span>👫</span>${data.population}</p>
+    <p class="country__row"><span>🗣️</span>${data.languages.eng}</p>
+    <p class="country__row"><span>💰</span>${data.currencies.KES.name}</p>
+  </div>
+</article>`;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
 });
